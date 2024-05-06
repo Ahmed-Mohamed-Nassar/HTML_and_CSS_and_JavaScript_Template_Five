2ab88a174b35bc09c016a091fa8af6e8469a256c
Ahmed Nassar <108141702+Ahmed-Mohamed-Nassar@users.noreply.github.com>
2024-05-06 08:37:54 -0700

    upload
    upload

---
+++ js/main2.js
@@ -0,0 +1,65 @@
+axios
+  .get(`http://api.alquran.cloud/v1/quran/quran-uthmani`)
+  .then(function (response) {
+    let mainUlForFehress = document.querySelector(".mainUlForFehres");
+    let allSewar = response.data.data.surahs;
+    // defult
+    let ataytexttdefult = document.querySelector(".ayatText");
+    let rakam = 0;
+    let elfat7a = response.data.data.surahs[0].ayahs;
+    console.log(elfat7a);
+    for (const aya of elfat7a) {
+      rakam++;
+      console.log(aya.text);
+      let span = document.createElement("span");
+      span.innerHTML = aya.text + ` <span class="rakam">${rakam}</span> `;
+      ataytexttdefult.appendChild(span);
+    }
+    // defult
+    let esmSoraAyatt = document.querySelector(".esmSoraAyat");
+    esmSoraAyatt.innerHTML = response.data.data.surahs[0].name;
+    let couuSora = 0;
+    for (const sora of allSewar) {
+      couuSora++;
+      let li = document.createElement("li");
+      li.innerHTML = `${couuSora}- <a href="#" Class="sora" >${sora.name}</a>`;
+      mainUlForFehress.appendChild(li);
+      let ayat = sora.ayahs;
+      let rakam = 0;
+    }
+    // defult
+    let defultActive = document.querySelectorAll(".sora");
+    defultActive[0].classList.add("active");
+    let aSora = document.querySelectorAll(".sora");
+    document.addEventListener("click", function (e) {
+      if (e.target.className == "sora") {
+        let theCurr = e.target;
+        defultActive.forEach(function (e) {
+          e.classList.remove("active");
+        });
+        theCurr.classList.add("active");
+        let last = response.data.data.surahs;
+        for (const las of last) {
+          if (theCurr.innerHTML == las.name) {
+            let ayatt = las.ayahs;
+            console.log(ayatt);
+            let ataytextt = document.querySelector(".ayatText");
+            ataytextt.innerHTML = "";
+            let rakam = 0;
+            esmSoraAyatt.innerHTML = las.name;
+            for (const aya of ayatt) {
+              rakam++;
+              console.log(aya.text);
+              let span = document.createElement("span");
+              span.innerHTML =
+                aya.text + ` <span class="rakam">${rakam}</span> `;
+              ataytextt.appendChild(span);
+            }
+          }
+        }
+      }
+    });
+  })
+  .catch(function (response) {
+    console.log(response);
+  });
---
+++ js/main3.js
@@ -0,0 +1,80 @@
+let mainArea = document.querySelector(".azkarText");
+let esmAzkar = document.querySelector(".esmAzkar");
+axios
+  .get(
+    "https://raw.githubusercontent.com/nawafalqari/azkar-api/56df51279ab6eb86dc2f6202c7de26c8948331c1/azkar.json"
+  )
+  .then(function (response) {
+    //
+    let elazkarToPut = response.data[`أذكار الصباح`];
+    esmAzkar.innerHTML = "أذكار الصباح";
+    //
+    for (const zekToPut of elazkarToPut) {
+      if (!Array.isArray(zekToPut) && zekToPut.content != "stop") {
+        let div = document.createElement("div");
+        div.className = "mainZikrTextBody";
+        div.innerHTML = `<span class="zekrTD">${
+          zekToPut.content
+        }</span> <span class="addElTekrar">عدد التكرار ${+zekToPut.count}</span>`;
+        mainArea.appendChild(div);
+      }
+    }
+    //
+    function getAzkar(ppp) {
+      return response.data[`${ppp}`];
+    }
+
+    let azkarMainName = [
+      "أذكار الصباح",
+      "أذكار المساء",
+      "أذكار الاستيقاظ",
+      "أذكار النوم",
+      "أذكار بعد السلام من الصلاة المفروضة",
+      "تسابيح",
+    ];
+
+    let couzkr = 0;
+    let azkarElmassa = getAzkar("أذكار المساء");
+    let askarMainUlForFehress = document.querySelector(".askarMainUlForFehres");
+
+    for (const zekrName of azkarMainName) {
+      couzkr++;
+      let li = document.createElement("li");
+      li.innerHTML = `${couzkr}- <a href="#" Class="zekr" >${zekrName}</a>`;
+      askarMainUlForFehress.appendChild(li);
+    }
+    let allzekrName = document.querySelectorAll(".zekr");
+    allzekrName[0].className += " active";
+    window.addEventListener("click", function (e) {
+      if (e.target.classList.contains("zekr")) {
+        let theCurr = e.target;
+        allzekrName.forEach(function (e) {
+          e.classList.remove("active");
+        });
+        theCurr.classList.add("active");
+        let theCurrSeleZekrName = theCurr.innerHTML;
+        putZekrContent(theCurrSeleZekrName);
+        esmAzkar.innerHTML = theCurrSeleZekrName;
+      }
+      function putZekrContent(para) {
+        mainArea.innerHTML = "";
+        let elazkarToPut = response.data[`${para}`];
+        for (const zekToPut of elazkarToPut) {
+          if (!Array.isArray(zekToPut) && zekToPut.content != "stop") {
+            let div = document.createElement("div");
+            div.className = "mainZikrTextBody";
+            div.innerHTML = `<span class="zekrTD">${
+              zekToPut.content
+            }</span> <span class="addElTekrar">عدد التكرار ${+zekToPut.count}</span>`;
+            mainArea.appendChild(div);
+          }
+        }
+      }
+      // addElTekrar
+    });
+  })
+  .catch(function (response) {
+    console.log(response);
+  });
+
+//
